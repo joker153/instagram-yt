ebb1b96f5d50cbe37e1ed79fcf88efa08dda5da8
joker153 <77005994+joker153@users.noreply.github.com>
2023-04-04 20:15:59 -1200

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,36 +14,42 @@ bot_token = os.environ.get("BOT_TOKEN")
 # Initialize the Pyrogram client
 app = Client("my_bot_token", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
-# Define the command to download Instagram videos
-@app.on_message(filters.command("insta"))
-async def insta(bot: Client, message: Message):
-    # Get the Instagram post URL from the message
-    url = message.text.split(" ")[1]
 
-    # Download the post using Instaloader
-    L = Instaloader()
-    post = Post.from_shortcode(L.context, url)
-    L.download_post(post, target="insta_videos")
+@app.on_message(filters.regex("(?i)https?://(?:www\.)?(?:instagram\.com|instagr\.am)/p/.*"))
+async def insta(client, message):
+    try:
+        url = message.matches[0].group(0)
+        insta = instaloader.Instaloader()
+        insta.download_video(url, target=f"{message.chat.id}.mp4")
+        await client.send_video(
+            chat_id=message.chat.id,
+            video=f"{message.chat.id}.mp4",
+            caption="Here is the requested video!",
+        )
+    except Exception as e:
+        await message.reply_text(f"An error occurred: {e}")
+    finally:
+        if os.path.exists(f"{message.chat.id}.mp4"):
+            os.remove(f"{message.chat.id}.mp4")
 
-    # Send the video file to the user
-    video_file = f"insta_videos/{post.date_utc.strftime('%Y-%m-%d_%H-%M-%S')}.mp4"
-    await bot.send_video(chat_id=message.chat.id, video=video_file)
 
-# Define the command to download YouTube videos
-@app.on_message(filters.command("youtube"))
-async def youtube(bot: Client, message: Message):
-    # Get the YouTube video URL from the message
-    url = message.text.split(" ")[1]
+@app.on_message(filters.regex("(?i)https?://(?:www\.)?(?:youtube\.com|youtu\.be)/.*"))
+async def youtube(client, message):
+    try:
+        url = message.matches[0].group(0)
+        yt = pytube.YouTube(url)
+        stream = yt.streams.get_highest_resolution()
+        stream.download(output_path=".", filename=f"{message.chat.id}.mp4")
+        await client.send_video(
+            chat_id=message.chat.id,
+            video=f"{message.chat.id}.mp4",
+            caption="Here is the requested video!",
+        )
+    except Exception as e:
+        await message.reply_text(f"An error occurred: {e}")
+    finally:
+        if os.path.exists(f"{message.chat.id}.mp4"):
+            os.remove(f"{message.chat.id}.mp4")
 
-    # Download the video using Pytube
-    video = YouTube(url)
-    stream = video.streams.get_highest_resolution()
-    stream.download(output_path="youtube_videos")
 
-    # Send the video file to the user
-    video_file = f"youtube_videos/{video.title}.mp4"
-    await bot.send_video(chat_id=message.chat.id, video=video_file)
-
-# Run the bot
-if __name__ == "__main__":
-    app.run()
+app.run()
