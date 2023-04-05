8aa9b67447ccc5a13ae97047ca19fdc0b00b6b01
joker153 <77005994+joker153@users.noreply.github.com>
2023-04-04 20:41:12 -1200

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,66 +11,45 @@ api_id = int(os.environ.get("API_ID"))
 api_hash = os.environ.get("API_HASH")
 bot_token = os.environ.get("BOT_TOKEN")
 
-# API endpoint and headers
-url = "https://aiov-download-youtube-videos.p.rapidapi.com/GetVideoDetails"
-headers = {
-    "X-RapidAPI-Key": "6923523517msha40bbd50901ebdfp19ae30jsn37c67364b1c0",
-    "X-RapidAPI-Host": "aiov-download-youtube-videos.p.rapidapi.com"
-}
-
-# Initialize the Pyrogram client
-app = Client("my_bot_token", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 
-# Start message
-@app.on_message(filters.command("start"))
-async def start(client, message):
-    await message.reply_text(
-        "Hello! I am a video downloader bot. Just send me a link to a YouTube, Instagram, Facebook, Snapchat, TikTok, Koo, or LinkedIn video, and I'll download it for you!"
-    )
+@app.on_message(pyrogram.filters.command(["start"]))
+def start(client, message):
+    message.reply_text("Hi, send me a YouTube or Instagram video link to download the video.")
 
+@app.on_message(pyrogram.filters.text)
+def download_video(client, message):
+    url = message.text
+    valid_platforms = ["youtube.com", "www.youtube.com", "m.youtube.com", "youtube.com.br", "www.youtube.com.br", "instagram.com", "www.instagram.com"]
+    if any(platform in url for platform in valid_platforms):
+        try:
+            video_details_url = "https://aiov-download-youtube-videos.p.rapidapi.com/GetVideoDetails"
+            headers = {
+                "X-RapidAPI-Key": "6923523517msha40bbd50901ebdfp19ae30jsn37c67364b1c0",
+                "X-RapidAPI-Host": "aiov-download-youtube-videos.p.rapidapi.com"
+            }
+            params = {"URL": url}
+            response = requests.get(video_details_url, headers=headers, params=params)
+            data = response.json()
 
-# Video downloader using API
-@app.on_message(filters.regex("(?i)https?://(?:www\.)?(?:youtube\.com|youtu\.be|instagram\.com|facebook\.com|snapchat\.com|tiktok\.com|koo\.app|linkedin\.com)/.*"))
-async def download_video(client, message):
-    try:
-        url = message.matches[0].group(0)
-        querystring = {"URL": url}
-        response = requests.request("GET", url, headers=headers, params=querystring)
-        data = response.json()
-
-        # Check if the video is available for download
-        if not data.get("DownloadInfo"):
-            await message.reply_text("Sorry, the video is not available for download.")
-            return
-
-        # Get the download URL and file extension
-        download_url = data["DownloadInfo"]["DownloadUrl"]
-        extension = data["DownloadInfo"]["Ext"]
-
-        # Download the video
-        filename = f"{message.chat.id}.{extension}"
-        response = requests.get(download_url, stream=True)
-        with open(filename, "wb") as file:
-            for chunk in response.iter_content(chunk_size=1024 * 1024):
-                if chunk:
-                    file.write(chunk)
-
-        # Send the downloaded video to the chat
-        caption = f"Title: {data['Title']}\nSize: {data['Size']}\nType: {data['Type']}"
-        await client.send_video(
-            chat_id=message.chat.id,
-            video=filename,
-            caption=caption,
-            thumb=data["ThumbnailUrl"],
-        )
-
-    except Exception as e:
-        await message.reply_text(f"An error occurred: {e}")
-
-    finally:
-        if os.path.exists(filename):
-            os.remove(filename)
-
+            video_url = data["StreamingURL"]
+            title = data["VideoTitle"]
+            file_type = data["FileType"]
+            thumb_url = data["ThumbnailURL"]
+            size = data["FileSize"]
+            filename = f"{title}.{file_type}"
+            if os.path.exists(filename):
+                message.reply_text("Video already exists.")
+            else:
+                message.reply_text(f"Downloading {title}...")
+                response = requests.get(video_url)
+                with open(filename, "wb") as f:
+                    f.write(response.content)
+                message.reply_video(video=filename, caption=title)
+                os.remove(filename)
+        except Exception as e:
+            message.reply_text(f"An error occurred: {str(e)}")
+    else:
+        message.reply_text("Invalid URL. Please send a valid YouTube or Instagram video link.")
 
 app.run()
